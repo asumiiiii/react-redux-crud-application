4b2ae52d4ad6dd3ae5c5805e548580761cd8fdf7
a_takahara <a_takahara@active.co.jp>
2021-05-26 14:16:01 +0900

    dom生成

--- src/App.js
+++ src/App.js
@@ -1,6 +1,12 @@
+import React from "react";
 
 function App() {
-  return <div><p>Hello, world!</p></div>;
+  return (
+    <React.Fragment>
+      <label htmlFor="bar">bar</label>
+      <input type="text" onChange={() => {console.log("I'm clicked.")}} />
+    </React.Fragment>
+  );
 }
 
 export default App;
