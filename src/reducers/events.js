2a53f542eddd4e2fcbd2ff91f0a025969f5c56f0
a_takahara <a_takahara@active.co.jp>
2021-08-30 00:40:27 +0900

    implement eventsShow

--- src/reducers/events.js
+++ src/reducers/events.js
@@ -1,10 +1,21 @@
 import _ from 'lodash';
-import { READ_EVENTS, DELETE_EVENTS } from '../actions'
+import { 
+    CREATE_EVENTS,
+    READ_EVENTS,
+    READ_EVENT,
+    UPDATE_EVENT,
+    DELETE_EVENTS,
+} from '../actions'
 
 export default (events = {}, action) => {
     switch (action.type) {
+        case CREATE_EVENTS:
         case READ_EVENTS:
+        case UPDATE_EVENT:
             return _.mapKeys(action.response.data, 'id')
+        case READ_EVENT:
+            const data = action.response.data
+            return { ...events, [data.id]: data }
         case DELETE_EVENTS:
             delete events[action.id]
             return { ...events }
